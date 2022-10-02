b57f057e64876688e867076dcab40ce4dff99b72
Tom Marius <tom@marius.consulting>
2022-10-02 11:53:07 +0200

    Add tooltip

--- src/components/globe/component.tsx
+++ src/components/globe/component.tsx
@@ -23,6 +23,9 @@ export function Component() {
                 hexPolygonColor={(i: any) => i?.properties?.color ?? '#fff'}
                 hexPolygonResolution={5}
                 hexPolygonMargin={0.1}
+                hexPolygonLabel={({ properties: p }: any) => {
+                    return `<div style="padding: 10px; background: rgba(0, 0, 0, 0.75); border-radius: 12px; border: 0.5px solid white;"><b>Means:</b><br />Temperature: <i>${p.temperature}</i><br />Irradiance: <i>${p.irradiance}</i><br />Wind speed: <i>${p.windSpeed}</i><br />Precipitation: <i>${p.precipitation}</i></div>`;
+                }}
             />
         </div>
     );
